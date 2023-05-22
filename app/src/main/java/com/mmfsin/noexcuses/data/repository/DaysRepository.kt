da63ae86078360707e837ed80465c24de411b649
Geraquem <geraquem@gmail.com>
2023-05-22 16:40:46 +0200

    wip

--- app/src/main/java/com/mmfsin/noexcuses/data/repository/DaysRepository.kt
+++ app/src/main/java/com/mmfsin/noexcuses/data/repository/DaysRepository.kt
@@ -1,9 +1,11 @@
 package com.mmfsin.noexcuses.data.repository
 
 import com.mmfsin.noexcuses.data.database.RealmDatabase
+import com.mmfsin.noexcuses.domain.models.ComboModel
 import com.mmfsin.noexcuses.domain.models.Day
 import com.mmfsin.noexcuses.domain.models.Phase
 import io.realm.kotlin.where
+import java.lang.Exception
 
 class DaysRepository {
 
@@ -17,6 +19,21 @@ class DaysRepository {
 
     fun addDay(day: Day): Boolean = realm.addObject { day }
 
-    /** borrar todos los combo models asociados a este dia */
-    fun deleteDay(day: Day): Boolean = realm.deleteObject({ day }, day.id)
+    fun deleteDay(day: Day): Boolean {
+        val dayId = day.id
+        return try {
+            deleteComboModels(dayId)
+            realm.deleteObject({ day }, dayId)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    private fun deleteComboModels(dayId: String) {
+        val comboModels = realm.getObjectsFromRealm {
+            where<ComboModel>().equalTo("dayId", dayId).findAll()
+        }
+        comboModels.forEach { cm -> realm.deleteObject({ cm }, cm.id) }
+    }
 }
--- app/src/main/java/com/mmfsin/noexcuses/data/repository/PhasesRepository.kt
+++ app/src/main/java/com/mmfsin/noexcuses/data/repository/PhasesRepository.kt
@@ -1,6 +1,8 @@
 package com.mmfsin.noexcuses.data.repository
 
 import com.mmfsin.noexcuses.data.database.RealmDatabase
+import com.mmfsin.noexcuses.domain.models.ComboModel
+import com.mmfsin.noexcuses.domain.models.Day
 import com.mmfsin.noexcuses.domain.models.Phase
 import io.realm.kotlin.where
 
@@ -14,6 +16,29 @@ class PhasesRepository {
 
     fun addPhase(phase: Phase): Boolean = realm.addObject { phase }
 
-    /** borrar todos los dias y combo models asociados a esta phase */
-    fun deletePhase(phase: Phase): Boolean = realm.deleteObject({ phase }, phase.id)
+    fun deletePhase(phase: Phase): Boolean {
+        val phaseId = phase.id
+        return try {
+            deleteDaysFromPhase(phaseId)
+            realm.deleteObject({ phase }, phaseId)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    private fun deleteDaysFromPhase(phaseId: String) {
+        val days = realm.getObjectsFromRealm {
+            where<Day>().equalTo("phaseId", phaseId).findAll()
+        }
+        days.forEach { day -> deleteComboModels(day.id) }
+        days.forEach { day -> realm.deleteObject({ day }, day.id) }
+    }
+
+    private fun deleteComboModels(dayId: String) {
+        val comboModels = realm.getObjectsFromRealm {
+            where<ComboModel>().equalTo("dayId", dayId).findAll()
+        }
+        comboModels.forEach { cm -> realm.deleteObject({ cm }, cm.id) }
+    }
 }
