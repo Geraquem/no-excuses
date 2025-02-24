fe19b912eb4d9719a95253cab60c7460c2c63b89
gers <geraquem@gmail.com>
2025-02-24 12:18:21 +0100

    wip

--- app/src/main/java/com/mmfsin/noexcuses/presentation/menu/actual/ActualExercisesFromMenuEvent.kt
+++ app/src/main/java/com/mmfsin/noexcuses/presentation/menu/actual/ActualExercisesFromMenuEvent.kt
@@ -1,7 +1,6 @@
 package com.mmfsin.noexcuses.presentation.menu.actual
 
 import com.mmfsin.noexcuses.domain.models.Day
-import com.mmfsin.noexcuses.presentation.myroutines.mexercises.MExercisesEvent
 
 sealed class ActualExercisesFromMenuEvent {
     class GetDay(val day: Day) : ActualExercisesFromMenuEvent()
