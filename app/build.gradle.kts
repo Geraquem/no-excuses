0e68fdba2c20f965a0651e10bd89d99c268c572e
gers <geraquem@gmail.com>
2026-06-17 18:06:51 +0200

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.mmfsin.noexcuses"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
